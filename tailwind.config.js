7b0060fe8620a5e0db97121e7f3f841e5af103d6
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-03 10:58:22 -0300

    Added font tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
         'brand_secondary': '#00B5E2',
         'brand_tertiary': '#75787B',
         'typography_primary': '#000000'
-      }
+      },
+      fontFamily: {
+        'text': ['Roboto', 'sans-serif'],
+        'highlight': ['Montserrat', 'sans-serif'] 
+      },
     },
   },
   variants: {
