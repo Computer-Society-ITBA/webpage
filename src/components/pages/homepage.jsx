7a8c456a375fc5df2a708c8f7709d8692d3aaa53
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-21 18:59:00 -0300

    Added mision / vision page

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 const Hero = React.lazy(() => import('../hero'));
 const Nosotros = React.lazy(() => import('../sections/nosotros'));
+const MisionVision = React.lazy(() => import('../sections/mision_vision'));
 
 class Homepage extends Component {
   render() {
     return <React.Fragment>
         <Hero/>
         <Nosotros/>
+        <MisionVision/>
     </React.Fragment>
   }
 }
--- src/i18n/en.js
+++ src/i18n/en.js
@@ -9,6 +9,12 @@ const i18n_en = {
         we_are_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempor tellus, nec mattis arcu. Fusce in leo eu felis bibendum posuere. Vestibulum vel porttitor lectus, eget dictum massa. Nullam commodo dui ligula, eget imperdiet justo fermentum vitae. Sed elementum tortor ornare urna venenatis pellentesque. Nam tempus, urna ut imperdiet hendrerit, nisl erat aliquam risus, a placerat justo est ac magna. Sed sed mauris pellentesque diam eleifend eleifend.',
         we_do: 'What do we do',
         we_do_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempor tellus, nec mattis arcu. Fusce in leo eu felis bibendum posuere. Vestibulum vel porttitor lectus, eget dictum massa. Nullam commodo dui ligula, eget imperdiet justo fermentum vitae. Sed elementum tortor ornare urna venenatis pellentesque. Nam tempus, urna ut imperdiet hendrerit, nisl erat aliquam risus, a placerat justo est ac magna. Sed sed mauris pellentesque diam eleifend eleifend.'
+    },
+    mision_vision: {
+        mision: 'Mision',
+        vision: 'Vision',
+        mision_text: '"Encourage interest and spread knowledge about technology"',
+        vision_text: '"Complement academic training with the use of current tools and technologies as the way to train better professionals"'
     }
 }
 
