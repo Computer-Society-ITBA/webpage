5328f0ab295b85d545ca39210dbb449ff17667e2
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-21 19:48:32 -0300

    Added stats section

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 const Hero = React.lazy(() => import('../hero'));
 const Nosotros = React.lazy(() => import('../sections/nosotros'));
+const Stats = React.lazy(() => import('../sections/stats'));
 
 class Homepage extends Component {
   render() {
     return <React.Fragment>
         <Hero/>
         <Nosotros/>
+        <Stats/>
     </React.Fragment>
   }
 }
