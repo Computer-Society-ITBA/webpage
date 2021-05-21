b3ce9a825e155af33a677dca9ba0dd90aad0b3aa
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-20 22:45:22 -0300

    Added section component

---
+++ src/components/section.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classlist from '../js/classlist';
+
+function Section (props) {
+    const alignment = props.textAlignment === 'left' ? 'text-left' : (props.textAlignment === 'right' ? 'text-right' : (props.textAlignment === 'center' ? 'text-center' : ''))
+    const bgColor = props.bgColor ? 'bg-' + props.bgColor : 'bg-white'
+    const padding = props.padding === 'large' ? 'px-24 py-32' : 'px-12 py-16'
+    const size = props.size === 'half' ? 'w-full h-screen-half' : 'h-screen w-full'
+    const flex = props.flex === 'col' ? 'flex flex-col' : (props.flex === 'row' ? 'flex flex-row' : '')
+
+    return (
+    <section className={classlist([
+        alignment,
+        bgColor,
+        padding,
+        size,
+        flex,
+        props.className
+        ])}>
+        {props.children}
+    </section>
+    )
+}
+
+export default Section; 
