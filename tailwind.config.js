15b6e2acc3886c3d5af0128fae3594df727e57c5
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-21 14:19:44 -0300

    Added missing whitelist patterns

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content:['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      whitelist: [/^bg-/]
+     }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
