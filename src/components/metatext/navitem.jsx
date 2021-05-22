ef217132ceddac4b56ce41c98e737431d749aeb4
Juan Oriana <joriana@itba.edu.ar>
2021-05-22 15:24:20 -0300

    modularized NavItems

---
+++ src/components/metatext/navitem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const NavItem = (props) => {
+  return (
+    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+      <a
+        href="/#"
+        class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
+      >
+        {props.children}
+      </a>
+    </motion.div>
+  );
+};
+
+export default NavItem;
