35e4b6aefec535da12d72868dbbc3ce59441a16f
Juan Oriana <joriana@itba.edu.ar>
2021-05-22 02:37:21 -0300

    useWindowDimensions hook for auto collapsing

--- src/components/metatext/navbar.jsx
+++ src/components/metatext/navbar.jsx
@@ -1,10 +1,13 @@
 import { React, useState } from "react";
 import logo from "../../images/logo_icon.png";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+
 function NavBar() {
-  const [collapsed, setCollapsed] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
+  const { width } = useWindowDimensions();
   return (
     <>
-      <nav class="flex items-center justify-between flex-wrap p-3 sticky top-0 z-50 bg-white bg-opacity-92">
+      <nav class="flex items-center justify-between flex-wrap p-3 sticky top-0 z-50 bg-white bg-opacity-90">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <a href="#">
             <img src={logo} alt="Computer Society Logo" class="w-12 ml-5" />
@@ -26,35 +29,35 @@ function NavBar() {
             </svg>
           </button>
         </div>
-        {!collapsed && 
-        <div class="w-full block flex-grow lg:background-black lg:mr-12 lg:flex lg:items-center lg:w-auto">
-          <div class=" font-highlight-semibold text-brand_primary  text-lg lg:flex-grow lg:flex lg:justify-end">
-            <a
-              href="#sect1"
-              class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
-            >
-              TEST
-            </a>
-            <a
-              href="#sect2"
-              class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
-            >
-              TEST
-            </a>
-            <a
-              href="#sect3"
-              class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
-            >
-              TEST
-            </a>
-            <a
-              href="#sect4"
-              class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
-            >
-              TEST
-            </a>
+        { (width > 1020 || !collapsed) &&
+          <div class="w-full block flex-grow lg:background-black lg:mr-12 lg:flex lg:items-center lg:w-auto">
+            <div class=" font-highlight-semibold text-brand_primary  text-lg lg:flex-grow lg:flex lg:justify-end">
+              <a
+                href="#sect1"
+                class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
+              >
+                TEST
+              </a>
+              <a
+                href="#sect2"
+                class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
+              >
+                TEST
+              </a>
+              <a
+                href="#sect3"
+                class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
+              >
+                TEST
+              </a>
+              <a
+                href="#sect4"
+                class="w-20 block lg:inline-block hover:text-brand_secondary mt-4 lg:mt-0 lg:mr-8 lg:w-auto"
+              >
+                TEST
+              </a>
+            </div>
           </div>
-        </div>
         }
       </nav>
     </>
