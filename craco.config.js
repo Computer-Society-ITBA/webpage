261e6f62cfc5d3a74669e4591c26e156eb83315b
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-22 22:06:02 -0300

    Minor uglify improvement

--- craco.config.js
+++ craco.config.js
@@ -8,7 +8,20 @@ module.exports = {
             warnings: false,
             drop_debugger: true,
             drop_console: true,
+            sequences: true,
+            dead_code: true,
+            conditionals: true,
+            booleans: true,
+            unused: true,
+            if_return: true,
+            join_vars: true
           },
+          output: {
+            comments: false,
+            beautify: false,
+            indent_level: 0
+          },
+          mangle: true
         },
         sourceMap: false,
         parallel: true,
