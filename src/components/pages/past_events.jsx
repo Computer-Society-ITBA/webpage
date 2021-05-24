96f55138280d3344248fe16987adbd387aa62032
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-24 18:23:48 -0300

    Removed unused

--- src/components/pages/past_events.jsx
+++ src/components/pages/past_events.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Footer = React.lazy(() => import('../sections/footer'));
-const Hero = React.lazy(() => import('../sections/hero'));
 const NavBar = React.lazy(() => import('../metatext/navbar'));
 const PastEventsFull = React.lazy(() => import('../sections/past_events_full'));
 
