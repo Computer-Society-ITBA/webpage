f787ebefc24714f6f5eea1e16fc5d9efca74c187
Gonzalo Hirsch <hirschgonzalo@gmail.com>
2021-05-27 19:44:26 -0300

    Added contact us section

---
+++ src/components/contact_form.jsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import i18n from '../i18n/index.js';
+
+const inputClass = "p-2 my-1 rounded-md bg-white text-brand_primary border-2 border-brand_primary focus:outline-none focus:border-black focus:border-2"
+
+function ContactForm () {
+    const [subject, setSubject] = useState("");
+    const [body, setBody] = useState("");
+  
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        window.location.href = `mailto:computersociety@itba.edu.ar?subject=${subject}&body=${body}`
+        setSubject("")
+        setBody("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col">
+            <input
+                id="subject"
+                name="subject"
+                type="text"
+                autoComplete="off"
+                value={subject}
+                placeholder={i18n.t('contact_us.form.subject')}
+                onChange={e => setSubject(e.target.value)}
+                className={inputClass}
+            />
+            <textarea 
+                cols="30" 
+                rows="7"
+                name="body"
+                id="body"
+                autoComplete="off"
+                style={{resize: 'none'}}
+                value={body}
+                onChange={e => setBody(e.target.value)}
+                placeholder={i18n.t('contact_us.form.body')}
+                className={inputClass}
+            />
+            <button 
+                type="submit" 
+                className="rounded-md mt-1 p-2 cursor-pointer bg-brand_primary text-typography_primary border-2 border-typography_primary transition duration-300 hover:text-white hover:bg-typography_primary focus:text-white focus:bg-typography_primary focus:outline-none"
+                >
+                    {i18n.t('contact_us.form.send')}
+            </button>
+        </form>
+    )
+}
+
+
+
+export default ContactForm; 
