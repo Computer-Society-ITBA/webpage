6d5203f9dacc5de2701b97438543bbeb29bfdb3d
Juan Oriana <joriana@itba.edu.ar>
2021-05-30 15:55:36 -0300

    Past events less pressed up

--- src/components/sections/past_events_full.jsx
+++ src/components/sections/past_events_full.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 // Translations
-import i18n from '../../i18n/index.js';
+import i18n from "../../i18n/index.js";
 
-import past_events from '../../data/past_events'
+import past_events from "../../data/past_events";
 
 // Components
-const Section = React.lazy(() => import('../section'));
-const PastEventCard = React.lazy(() => import('../past_event_card'));
+const Section = React.lazy(() => import("../section"));
+const PastEventCard = React.lazy(() => import("../past_event_card"));
 
-function PastEvents () {
-  return (<Section bgColor="bg-white" textAlignment="center">
-      <h2>{i18n.t('past_events.title')}</h2>
+function PastEvents() {
+  return (
+    <Section bgColor="bg-white" textAlignment="center">
+      <h2 className="pt-12">{i18n.t("past_events.title")}</h2>
       <div className="flex flex-col sm:flex-row sm:flex-wrap h-full w-full">
-        {
-          past_events.map((event, index) => {
-            return (
-              <PastEventCard event={event} index={index}/>
-            )
-          })
-        }
+        {past_events.map((event, index) => {
+          return <PastEventCard event={event} index={index} />;
+        })}
       </div>
-    </Section>)
+    </Section>
+  );
 }
 
 export default PastEvents;
