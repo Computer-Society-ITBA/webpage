df55df667f2b31a08931e0edf757a5b341f90ec9
Gonzalo Hirsch <40722245+GonzaloHirsch@users.noreply.github.com>
2021-06-25 14:24:26 -0300

    Hotfix: Fixed typo in mission

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -31,7 +31,7 @@ const i18n_en = {
     },
   },
   mision_vision: {
-    mision: "Mision",
+    mision: "Mission",
     vision: "Vision",
     mision_text: '"Spark interest and spread knowledge about technology-related topics."',
     vision_text:
