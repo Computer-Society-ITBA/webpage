f1651e353a2877bb9cf317c09f38ff90f4c53261
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-10-22 17:06:27 -0300

    get past events from firebase

--- src/components/sections/past_events.jsx
+++ src/components/sections/past_events.jsx
@@ -4,44 +4,36 @@ import i18n from '../../i18n/index.js';
 
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 
-import past_events from '../../data/past_events'
-
-const filterNEvents = (n, events) => {
-  events.sort((a, b) => {
-    if(a.attendants === b.attendants) {
-      return b.attendants;
-    } else {
-      return b.attendants - a.attendants;
-    }
-  });
-  return events.slice(0, n)
-} 
-const past_events_copy = [...past_events]
+import usePastEvents from '../../hooks/usePastEvents.js';
 
 // Components
 const Section = React.lazy(() => import('../section'));
 const LinkButton = React.lazy(() => import('../link_button'));
 const PastEventCard = React.lazy(() => import('../past_event_card'));
 
-function PastEvents () {
+function PastEvents() {
   const { width } = useWindowDimensions();
   const n = width >= 768 ? 6 : (width >= 640 ? 4 : 2)
-  const filteredEvents = filterNEvents(n, past_events_copy)
-  return (<Section id="past-events" bgColor="bg-white" textAlignment="center">
+
+  const pastEvents = usePastEvents({ orderBy: "attendants", direction: "desc", limit: n });
+
+  return (
+    <Section id="past-events" bgColor="bg-white" textAlignment="center">
       <h2>{i18n.t('past_events.title')}</h2>
       <div className="flex flex-col sm:flex-row sm:flex-wrap h-full w-full">
         {
-          filteredEvents.map((event, index) => {
-            return (
-              <PastEventCard key={index} event={event} index={index}/>
-            )
-          })
+          pastEvents.length == 0 ?
+            <div className="loader" /> :
+            pastEvents.map((event, index) => {
+              return <PastEventCard key={index} event={event} index={index} />
+            })
         }
       </div>
       <div className="flex flex-row w-full justify-center items-center mt-4">
-      <LinkButton href="/past-events/#" type="outlined-dark" text={i18n.t('past_events.button')}/>
+        <LinkButton href="/past-events/#" type="outlined-dark" text={i18n.t('past_events.button')} />
       </div>
-    </Section>)
+    </Section>
+  )
 }
 
 export default PastEvents;
---
+++ src/hooks/usePastEvents.js
@@ -0,0 +1,41 @@
+import { collection, getDocs, limit as fLimit, orderBy as fOrderBy, query as fQuery } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import i18n from "../i18n";
+import { db } from "../firebase";
+
+export default function usePastEvents({ orderBy, direction, limit }) {
+    if (direction !== "asc" && direction !== "desc") direction = "asc";
+
+    const [pastEvents, setPastEvents] = useState([]);
+
+    useEffect(() => {
+        async function getPastEvents() {
+            const pastEventsRef = collection(db, "past_events");
+
+            console.log(direction)
+
+            let query;
+            if (limit > 0)
+                query = fQuery(pastEventsRef, fOrderBy(orderBy, direction), fLimit(limit))
+            else
+                query = fQuery(pastEventsRef, fOrderBy(orderBy, direction))
+
+            // Get current browser's language
+            let language = i18n.language.split("-")[0];
+            if (language !== "en" || language !== "es") language = "en";
+
+            const result = await getDocs(query);
+            const data = result.docs.map((doc) => {
+                const data = doc.data();
+                return {
+                    "attendants": data.attendants,
+                    ...data[language],
+                };
+            });
+            setPastEvents(data);
+        }
+        getPastEvents();
+    }, []);
+
+    return pastEvents;
+}
