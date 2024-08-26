cb7eeb5ae54bf6017ede4208ebaa17425485c94f
Clara Rodriguez Acevedo <78107786+clararodriguezacevedo@users.noreply.github.com>
2024-08-26 20:38:20 -0300

    fix-carousel-commit

--- src/components/sections/sponsors.jsx
+++ src/components/sections/sponsors.jsx
@@ -10,35 +10,30 @@ const sponsorImages = import.meta.glob("../../images/sponsors/*", { eager: true,
 
 // Components
 const Section = React.lazy(() => import('../section'));
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Marquee from "react-fast-marquee";
 
 function Sponsors() {
-	const settings = {
-		infinite: true,
-		slidesToShow: 4,
-      	slidesToScroll: 1,
-		adaptiveHeight: true,
-		autoplay: true,
-		speed: 4100,
-		autoplaySpeed: 0,
-		cssEase: "linear",
-	}
-	return (
-		<Section id='our-sponsors' bgColor='bg-white' textAlignment='center'>
-			<h2>{i18n.t('sponsors.title')}</h2>
-			<Slider {...settings}>
-				{sponsors.map((sponsor, index) => (
-					<div key={index} className="py-10 px-5 mx-10" >	
-					<a href={sponsor.link} rel='noreferrer' target='_blank'>
-						<img src={sponsorImages[`../../images/sponsors/${sponsor.src}`]} alt={i18n.t(sponsor.alt)} className=" h-24 my-0 mx-auto"/>
-					</a>	
-					</div>
-				))}
-			</Slider>
-		</Section>
-	);
+    const settings = {
+        gradient: "true",
+        direction: "left",
+        speed: 70,
+    }
+
+    return (
+        <Section id="our-sponsors" bgColor='bg-white' textAlignment='center'>
+            <h2>{i18n.t('sponsors.title')}</h2>
+            <Marquee {...settings}>
+                {sponsors.map((sponsor, index) => (
+                    <div key={index} className="py-10 px-5 mx-10" >
+                        <a href={sponsor.link} rel='noreferrer' target='_blank'>
+                            <img src={sponsorImages[`../../images/sponsors/${sponsor.src}`]} alt={i18n.t(sponsor.alt)} className=" h-16 my-0 mx-auto" />
+                        </a>
+                    </div>
+                ))}
+            </Marquee>
+        </Section>
+    )
 }
+
 
 export default Sponsors;
